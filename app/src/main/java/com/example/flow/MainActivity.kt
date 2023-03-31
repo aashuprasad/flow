fbcff7ae13140b3e5b2a00a692e90df29472b196
Ashu Prasad <aashuprasad@live.com>
2023-03-31 16:18:42 +0530

    channel impl

--- app/src/main/java/com/example/flow/MainActivity.kt
+++ app/src/main/java/com/example/flow/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.flow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    val channel:Channel<Int>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        producer()
+        consumer()
+    }
+
+    fun producer(){
+        CoroutineScope(Dispatchers.Main).launch {
+            channel.send(1)
+            channel.send(2)
+        }
+    }
+
+    fun consumer(){
+        CoroutineScope(Dispatchers.Main).launch {
+            Log.d("KOTLIN_DEBUG", channel.receive().toString())
+            Log.d("KOTLIN_DEBUG", channel.receive().toString())
+
+        }
     }
 }
